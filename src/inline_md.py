815899dcfd0c611d48a96168b43223ab6d81e495
teh-deadpixel <shroffharsh2@gmail.com>
2026-04-05 15:27:26 +0000

    survived the great cache betrayal: markdown to html, pycache dealt with accordingly

--- src/inline_md.py
+++ src/inline_md.py
@@ -74,5 +74,10 @@ def split_nodes_link(old_nodes):
                 new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
 
-
-
+def text_to_textnodes(text):
+    result = split_nodes_delimiter([TextNode(text, TextType.TEXT)], "`", TextType.CODE)
+    result = split_nodes_delimiter(result, "**", TextType.BOLD)
+    result = split_nodes_delimiter(result, "_", TextType.ITALIC)
+    result = split_nodes_image(result)
+    result = split_nodes_link(result)
+    return result
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,7 @@
 from enum import Enum
-
+from htmlnode import PARENTNODE, LEAFNODE
+from inline_md import text_to_textnodes
+from textnode import text_node_to_html_node, TextNode, TextType
 def markdown_to_blocks(markdown):
     splitted = markdown.split("\n\n")
     stripped = []
@@ -49,3 +51,92 @@ def block_to_block_type(md):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    html_nodes = []
+    for text_node in text_nodes:
+        html_nodes.append(text_node_to_html_node(text_node))
+    return html_nodes
+    
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    joined_lines = " ".join(lines)
+    html_nodes = text_to_children(joined_lines)
+    parent = PARENTNODE("p", html_nodes)
+    return parent
+
+def heading_to_html_node(block):
+    hashes = 0
+    for ch in block:
+        if ch == "#":
+            hashes += 1
+        else:
+            break
+    stripped = block[hashes+1:]
+    html_nodes = text_to_children(stripped)
+    parent = PARENTNODE(f"h{hashes}", html_nodes)
+    return parent
+
+def code_to_html_node(block):
+    stripped = block[4:-3]
+    text_node = TextNode(stripped, TextType.TEXT)
+    html_node = text_node_to_html_node(text_node)
+    child  = PARENTNODE("code", [html_node])
+    parent = PARENTNODE("pre", [child])
+    return parent
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    stripped = []
+    for line in lines:
+        stripped.append(line[1:])
+    joined_lines = " ".join(stripped)
+    html_node = text_to_children(joined_lines)
+    parent = PARENTNODE("blockquote", html_node)
+    return parent
+
+def ulist_to_html_node(block):
+    lines = block.split("\n")
+    child = []
+    for line in lines:
+        stripped = line[2:]
+        c_nodes = text_to_children(stripped)
+        child.append(PARENTNODE("li", c_nodes))
+    parent = PARENTNODE("ul", child)
+    return parent
+
+def olist_to_html_node(block):
+    lines = block.split("\n")
+    child = []
+    for line in lines:
+        dot_split = line.split(". ")
+        stripped = dot_split[1]
+        html_node = text_to_children(stripped)
+        child.append(PARENTNODE("li", html_node))
+    parent = PARENTNODE("ol", child)
+    return parent
+
+def block_to_html_node(block):
+    
+    
+    block_type = block_to_block_type(block)
+    if block_type == BlockType.HEADING:
+        return heading_to_html_node(block)
+    if block_type == BlockType.PARAGRAPH:
+        return paragraph_to_html_node(block)
+    if block_type == BlockType.CODE:
+        return code_to_html_node(block)
+    if block_type == BlockType.QUOTE:
+        return quote_to_html_node(block)
+    if block_type == BlockType.UNORDERED_LIST:
+        return ulist_to_html_node(block)
+    if block_type == BlockType.ORDERED_LIST:
+        return olist_to_html_node(block)
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        children.append(block_to_html_node(block))
+    return PARENTNODE("div", children)
