ba49fa5b1e75ad9390087d9f1fb809d7990dca21
teh-deadpixel <shroffharsh2@gmail.com>
2026-04-11 10:16:12 +0000

    taught my program to clean up after itself before redecorating

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
 from textnode import TextNode
 from textnode import TextType
+import os
+import shutil
+
+def copy_static(source, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for entry in os.listdir(source):
+        destination_path = os.path.join(destination, entry)
+        source_path = os.path.join(source, entry)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path)
+            print(f"file copied: {destination_path} from {source_path}")
+        else:
+            copy_static(source_path, destination_path)
+
+
 def main():
-    node =  TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    #node =  TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    #print(node)
+    source = "static"
+    destination = "public"
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    copy_static(source, destination)
 main()
+
